61a448ac2633e0cc71fa939a036d9115b6411fd8
EdwinSch <edwinschutjes@gmail.com>
2023-08-25 11:37:46 +0200

    prj init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import notificationData from "./data";
+
+function App() {
+  console.log(notificationData);
+
+  return (
+    <>
+      <h2>hello world</h2>
+    </>
+  );
+}
+
+export default App;
