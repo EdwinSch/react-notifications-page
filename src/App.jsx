0f2309fe131c6be66d92703848baf48c1301268b
EdwinSch <edwinschutjes@gmail.com>
2023-08-28 10:37:46 +0200

    count all unread items

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import notifications from "./data";
 
 import Header from "./components/Header";
 import NotificationsList from "./components/NotificationsList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [data, setData] = useState(notifications);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    countNewItems();
+  }, []);
 
   // Mark single notification as read
   const markRead = (id) => {
@@ -17,21 +22,31 @@ function App() {
       return item;
     });
     setData(newItems);
+    countNewItems();
   };
 
-  // Mark aLL notifications as read
+  // Mark ALL notifications as read
   const markAll = () => {
     const newItems = data.map((item) => {
       const newItem = { ...item, newPost: false };
       return newItem;
     });
     setData(newItems);
+    countNewItems();
+  };
+
+  // Count unread notifications
+  const countNewItems = () => {
+    const unread = data.filter((items) => {
+      return items.newPost === true;
+    });
+    setCounter(unread.length);
   };
 
   // RENDER app
   return (
     <main>
-      <Header data={data} markAll={markAll} />
+      <Header markAll={markAll} counter={counter} />
       <NotificationsList data={data} markRead={markRead} />
     </main>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-const Header = ({ markAll }) => {
+const Header = ({ markAll, counter }) => {
   return (
     <header>
       <section className="title">
         <h1>notifications</h1>
-        <div className="display-new">3</div>
+        <div className="display-new">{counter}</div>
       </section>
       <button onClick={markAll} className="text-btn" type="button">
         Mark all as read
